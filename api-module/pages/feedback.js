cf2c44be67261af5a29010c76976545e6bbea1f0
MarcGames1 <George.marcu20@gmail.com>
2021-11-07 16:44:31 +0200

    147

---
+++ api-module/pages/feedback.js
@@ -0,0 +1,45 @@
+import { buildFeedbackPath, extractFeedback } from './api/feedback';
+import {useState, Fragment} from 'react'
+
+function FeedbackPage(props) {
+
+const [feedbackData, setFeedbackData] = useState()
+
+  function LoadFeedbackHandler(id) {
+    fetch(`/api/${id}`)
+    .then((response) => response.json())
+    .then((data) =>{
+      setFeedbackData(data.feedback)
+      
+    })
+  }
+
+  
+  return (
+    <Fragment>
+     {feedbackData && <p>{feedbackData.email}</p>}
+      <ul>
+        {props.feedbackItems.map((item) => (
+          <li key={item.id}>
+            {item.text}
+            <button onClick={LoadFeedbackHandler.bind(null, item.id)}>
+              Show Details
+            </button>
+          </li>
+        ))}
+      </ul>
+    </Fragment>
+  );
+}
+
+export async function getStaticProps() {
+    const filePath =  buildFeedbackPath()
+    const data =  extractFeedback(filePath)
+    return {
+        props:{
+            feedbackItems:data,
+        },
+    }
+}
+
+export default FeedbackPage;
--- api-module/pages/index.js
+++ api-module/pages/index.js
@@ -1,7 +1,65 @@
-const homepage =()=>{
+import { useRef, useState } from 'react';
 
 
-return <h1>hello</h1>
-}
 
-export default homepage
+
+const HomePage = () => {
+  const [feedbackItems, setFeedbackItems] = useState([]);
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+  function loadFeedbackHandler() {
+    fetch('/api/feedback')
+      .then((response) => response.json())
+      .then((data) => setFeedbackItems(data.feedback));
+  }
+
+  function submitFormHandler(event) {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredFeedback = feedbackInputRef.current.value;
+    const reqBody = {
+      email: enteredEmail,
+      text: enteredFeedback,
+    };
+
+    fetch('/api/feedback', {
+      method: 'POST',
+      body: JSON.stringify(reqBody),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }
+
+  return (
+    <div>
+      <h1>Homepage</h1>
+      <form onSubmit={submitFormHandler}>
+        <div>
+          <label htmlFor="email">Your Email address</label>
+          <input type="email" id="email" ref={emailInputRef} />
+        </div>
+        <div>
+          <label htmlFor="feedback">Your Feedback</label>
+          <textarea
+            type="feedback"
+            id="feedback"
+            rows="5"
+            ref={feedbackInputRef}
+          ></textarea>
+        </div>
+        <button>Send feedback</button>
+      </form>
+      <hr />
+      <button onClick={loadFeedbackHandler}>load Feedback</button>
+      <ul>
+          {feedbackItems.map(item => <li key={item.id}>{item.text} :::___::: {item.email}</li>)}
+      </ul>
+    </div>
+  );
+};
+
+export default HomePage;
